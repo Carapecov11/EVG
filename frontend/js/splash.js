f28a34b82ecbaf25b04db68135a29ac3ec47fb26
Rodrigo Carapecov Mota <rcarapecov@gmail.com>
2026-08-08 19:50:07 -0300

    Atualiza projeto EVG

--- frontend/js/splash.js
+++ frontend/js/splash.js
@@ -1,19 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    console.log("SPLASH INICIADA");
+
     const splash = document.querySelector(".splash");
 
     if (!splash) {
-        window.location.href = "login.html";
+        console.log("SPLASH NÃO ENCONTRADA");
         return;
     }
 
+    console.log("SPLASH ENCONTRADA");
+
     setTimeout(() => {
+
+        console.log("INICIANDO SAÍDA");
 
         splash.classList.add("sair");
 
         setTimeout(() => {
 
-            window.location.replace("login.html");
+            console.log("INDO PARA LOGIN");
+
+            window.location.href = "login.html";
 
         }, 800);
 
